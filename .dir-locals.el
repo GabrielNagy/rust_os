561877d63ed705fec0e19ff50b223494e3bb2030
Gabriel Nagy <gabrielnagy@me.com>
2019-12-23 01:44:06 +0200

    Complete part 2

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((rust-mode
+  (lsp-rust-all-targets nil)
+  (cargo-process--command-build . "xbuild")
+  (cargo-process--command-run . "xrun")
+  (lsp-rust-sysroot . "target/sysroot")))
+
